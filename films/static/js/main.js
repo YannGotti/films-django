9ac5e5ce14d3c2a186714e1a6290e16bc4ba4291
mironovv <1111111111aleksey46@gmail.com>
2023-02-12 00:14:32 +0300

    add description film

--- films/static/js/main.js
+++ films/static/js/main.js
@@ -1,14 +1,13 @@
 TOKEN = 'e599f1f6f2ecd77c39ab9f9b6aac1b28'
 
 
-
 function addFilmPage(film) {
     info = film.info
 
     let row = document.getElementById("film-list");
 
     row.innerHTML += 
-    `<a href=film/` + parseInt(film.kinopoisk_id) + `/ class="col-6 col-lg-2 animate__animated animate__fadeIn films-link ">
+    `<a href=film/` + parseInt(film.kinopoisk_id) + `/ class="col-6 col-lg-2 animate__animated animate__fadeIn films-link">
         <div class="card text-white bg-dark mb-3" style="max-width: 14rem; height: 23rem;">
             <img style="max-width: 14rem; max-height: 17rem;" src="`+ info.poster +`" class="card-img-top">
             <div class="card-body">
